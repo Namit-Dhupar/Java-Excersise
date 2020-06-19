330f9b6be0d7d051663319ba9b7bac2c12636b36
Namit-Dhupar <namitdhupar2238@gmail.com>
2020-06-20 00:07:55 +0530

    arrays

---
+++ Java Practice/Java Array/MultiDimension.java
@@ -0,0 +1,15 @@
+
+public class MultiDimension {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] arr; //first bracket for row, second for column
+		arr = new int[5][];//specifying rows = 5
+		//Adding Columns = 4
+		for(int i=0 ; i<arr.length ; i++) {
+			arr[i] = new int[4];
+		}
+
+	}
+
+}
